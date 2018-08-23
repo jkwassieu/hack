8149edb1415dcbd2996d1426a751610cd218b416
jkwassieu <42658826+jkwassieu@users.noreply.github.com>
2018-08-23 17:57:46 -0500

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,17 @@
+/**
+
+ * Author: Judith Kwassieu Computer Science
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Judith Kwassieu Computer Science!\n");
+
+  return 0;
+}
